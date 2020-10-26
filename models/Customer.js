347303e6de4295f7de601125bbbd0c73912dc8ae
Sai Akshith V <akshith292@gmail.com>
2020-10-26 23:27:39 +0530

    questionnaire

--- models/Customer.js
+++ models/Customer.js
@@ -1,3 +1,4 @@
+const { Decimal128, Double } = require('mongodb');
 var mongoose = require('mongoose');
 
 var CustomerSchema = new mongoose.Schema({
@@ -16,15 +17,100 @@ var CustomerSchema = new mongoose.Schema({
 	longitude: {
 		type: Number
 	},
-	service_location: {
+	// service_location: {
+	// 	type: String
+	// },
+	area_of_service1: {
 		type: String
 	},
-	purpose_of_service: {
+	area_of_service2: {
 		type: String
 	},
-	Service: {
-		type:String
+	area_of_service3: {
+		type: String
+	},
+	area_of_service4: {
+		type: String
+	},
+	area_of_service5: {
+		type: String
+	},
+	area_of_service6: {
+		type: String
+	},
+	area_of_service7: {
+		type: String
+	},
+	area_of_service8: {
+		type: String
+	},
+	carpenterTime: {
+		type: Number,
+		default:0
+	},
+	carpenter: {
+		type: Number,
+		default:0
+	},
+	plumberTime: {
+		type: Number,
+		default:0
+	},
+	plumber: {
+		type: Number,
+		default:0
+	},
+	electricianTime: {
+		type: Number,
+		default:0
+	},
+	electrician: {
+		type: Number,
+		default:0
+	},
+	garbage_collectorTime: {
+		type: Number,
+		default: 0
+	},
+	garbage_collector: {
+		type: Number,
+		default:0
+	},
+	pestcontrollerTime: {
+		type: Number,
+		default:0
+	},
+	pestcontroller: {
+		type: Number,
+		default:0
+	},
+	barberTime: {
+		type: Number,
+		default:0
+	},
+	barber: {
+		type: Number,
+		default:0
+	},
+	cleanerTime: {
+		type: Number,
+		default:0
+	},
+	cleaner: {
+		type: Number,
+		default:0
+	},
+	painterTime: {
+		type: Number,
+		default:0
+	},
+	painter: {
+		type: Number,
+		default:0
 	}
+	// Service: {
+	// 	type:String
+	// }
 	// RepairAndRestoration: {
 	// 	type: String
 	// },
--- routes/customer.js
+++ routes/customer.js
@@ -13,15 +13,68 @@ router.post('/Customer', function(req, resp) {
 	var identity = req.body.identity;
 	var longitude = req.body.longitude;
 	var latitude = req.body.latitude;
-	var service_location = req.body.service_location;
-	var purpose_of_service = req.body.purpose_of_service;
-	var Service= req.body.Service;
+	// var service_location = req.body.service_location;
+	var area_of_service1 = req.body.area_of_service1;
+	var area_of_service2 = req.body.area_of_service2;
+	var area_of_service3 = req.body.area_of_service3;
+	var area_of_service4 = req.body.area_of_service4;
+	var area_of_service5 = req.body.area_of_service5;
+	var area_of_service6 = req.body.area_of_service6;
+	var area_of_service7 = req.body.area_of_service7;
+	var area_of_service8 = req.body.area_of_service8;
+	var carpenterTime = req.body.carpenterTime;
+	var carpenter = req.body.carpenter;
+	var plumberTime = req.body.plumberTime;
+	var plumber = req.body.plumber;
+	var electricianTime = req.body.electricianTime;
+	var electrician = req.body.electrician;
+	var garbage_collectorTime = req.body.garbage_collectorTime;
+	var garbage_collector = req.body.garbage_collector;
+	var pestcontrollerTime = req.body.pestcontrollerTime;
+	var pestcontroller = req.body.pestcontroller;
+	var barberTime = req.body.barberTime;
+	var barber = req.body.barber;
+	var cleanerTime = req.body.cleanerTime;
+	var cleaner = req.body.cleaner;
+	var painterTime = req.body.painterTime;
+	var painter = req.body.painter;
+	// var Service= req.body.Service;
 	// var RepairAndRestoration = req.body.RepairAndRestoration;
 	// var ProductionOrInstallation = req.body.ProductionOrInstallation;
 	// var SanitationAndCleaning = req.body.SanitationAndCleaning;
 	// var SelfCare = req.body.SelfCare;
-	// RepairAndRestoration, ProductionOrInstallation: ProductionOrInstallation, SanitationAndCleaning: SanitationAndCleaning, SelfCare: SelfCare
-	var newCustomer = { name: name, contact_no: contact_no, identity: identity, longitude: longitude, latitude: latitude, service_location: service_location, purpose_of_service: purpose_of_service,Service:Service  };
+	// RepairAndRestoration, ProductionOrInstallation: ProductionOrInstallation, SanitationAndCleaning: SanitationAndCleaning, SelfCare: SelfCare,Service:Service,service_location: service_location,
+	var newCustomer = {
+		name: name,
+		contact_no: contact_no,
+		identity: identity,
+		longitude: longitude,
+		latitude: latitude,
+		area_of_service1: area_of_service1,
+		area_of_service2: area_of_service2,
+		area_of_service3: area_of_service3,
+		area_of_service4: area_of_service4,
+		area_of_service5: area_of_service5,
+		area_of_service6: area_of_service6,
+		area_of_service7: area_of_service7,
+		area_of_service8: area_of_service8,
+		carpenterTime: carpenterTime,
+		carpenter:carpenter,
+		plumberTime: plumberTime,
+		plumber:plumber,
+		electricianTime: electricianTime,
+		electrician: electrician,
+		garbage_collectorTime: garbage_collectorTime,
+		garbage_collector:garbage_collector,
+		pestcontrollerTime: pestcontrollerTime,
+		pestcontroller:pestcontroller,
+		barberTime: barberTime,
+		barber:barber,
+		cleanerTime: cleanerTime,
+		cleaner: cleaner,
+		painterTime: painterTime,
+		painter:painter
+	};
 	Customer.create(newCustomer, function(err, newlyCreated) {
 		if (err) {
 			console.log(err);
